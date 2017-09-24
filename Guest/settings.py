d08437202821246af73ace9a5636226aa8d2cc1e
liu1993 <liujiao0910@163.com>
2017-09-24 23:41:12 +0800

    添加bootstrap3应用

--- Guest/settings.py
+++ Guest/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sign',#添加sign应用
+    'sign',#添加sign应用x
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
